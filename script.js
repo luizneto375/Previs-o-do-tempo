acd4004b9a1571acecc2a6b056eb147e579dc814
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-04-25 10:49:27 -0300

    atualizando a função, pra mostrar o tempo, de acordo com o estado, que foi feito a pesquisa

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const key = "e5d1c0a8fa9b2d67d17a291407ddcf0b"
 
 function colocarDadosNatela(dados) {
     console.log(dados)
-
+    document.querySelector(".tempo").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".nublado-2").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".nublado").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
 async function buscarCidade(cidade) {
-    const dados = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then( resposta => resposta.json())
+    const dados = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
    
     colocarDadosNatela(dados)
 }
