7f4591297bf5007829cd4689418f38d02cea8e5c
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-04-25 09:41:01 -0300

    criando função para uma previa da previsão do tempo atual

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 
 const key = "e5d1c0a8fa9b2d67d17a291407ddcf0b"
 
+function colocarDadosNatela(dados) {
+    console.log(dados)
 
+}
 
+async function buscarCidade(cidade) {
+    const dados = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then( resposta => resposta.json())
+   
+    colocarDadosNatela(dados)
+}
 
 function cliqueiNoBotao() {
-    const input = document.querySelector(".digite").value
+    const cidade = document.querySelector(".digite").value
 
-    console.log(input)
+    buscarCidade(cidade)
 }
